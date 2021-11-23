63bdd87324a5e7ed7890df40304cdf2b4028af70
RajuRk <45646335+RajuRk@users.noreply.github.com>
2021-11-23 22:59:57 +0530

    Update Addstudent.js

--- src/components/Addstudent.js
+++ src/components/Addstudent.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Addstudent() {
+  
+  const data = [{
+      name: "Raju",
+      grad: "B.E",
+      dept: "Electronics",
+      mobile: "1236548978",
+      email: "rajkumar@gmail.com",
+    },
+    {
+      name: "Kumaran",
+      grad: "B.E",
+      dept: "Computer Science",
+      mobile: "9876548587",
+      email: "kumaran@gmail.com",
+    }];
+
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [mobile, setMobile] = useState("");
+  let [grad, setGrad] = useState("");
+  let [dept, setDept] = useState("");
+  
+  let hanldeSave = () =>{
+    data.push({
+      name,
+      email,
+      mobile,
+      grad,
+      dept
+    })
+
+    console.log(data)
+  }
+ 
   return (
     <div>
-      <h1>Add Student</h1>
+      <h3>Add Student</h3>
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Mobile"
+            onChange={(e) => setMobile(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Grade" 
+            onChange={(e) => setGrad(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Department"
+            onChange={(e) => setDept(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
